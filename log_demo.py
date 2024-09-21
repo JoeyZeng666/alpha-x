f80d936dd88e120983f5d691be24e1ece531fbba
Souprice <0706zengyan@gmail.com>
2024-09-21 11:35:34 +0800

    使用poetry 管理环境

---
+++ log_demo.py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+import time
+
+from tencentcloud.log.cls_pb2 import LogGroupList
+from tencentcloud.log.logclient import LogClient
+from tencentcloud.log.logexception import LogException
+
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def upload(topic_id, client):
+    LogLogGroupList = LogGroupList()
+    LogGroup = LogLogGroupList.logGroupList.add()
+    LogGroup.filename = "python.log"
+    LogGroup.source = "127.0.0.1"
+
+    LogTag = LogGroup.logTags.add()
+    LogTag.key = "key"
+    LogTag.value = "value"
+
+    Log = LogGroup.logs.add()
+    Log.time = int(round(time.time() * 1000000))
+
+    Content = Log.contents.add()
+    Content.key = "Hello"
+    Content.value = "World"
+    try:
+        request = client.put_log_raw(topic_id, LogLogGroupList)
+        print(request.get_request_id())
+    except LogException as e:
+        print(e)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    endpoint = 'https://ap-guangzhou.cls.tencentcs.com'
+    accessKeyId = '1253534728'
+    accessKey = 'AKIDe8TaU9eVCyWqysX0GJ8k8JGy1wfAQK9l'
+    topic_id = 'a8080cef-2d84-4291-a3ee-555c3d70facc'
+    client = LogClient(endpoint, accessKeyId, accessKey)
+    upload(topic_id, client)
